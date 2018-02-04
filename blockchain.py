83f5f6718eee7e5f93200ac0eb5ff654b9444efe
tacnoma <ctchnm.geo@gmail.com>
2018-02-04 15:13:06 +0900

    Adds skelton blockchain class

---
+++ blockchain.py
@@ -0,0 +1,25 @@
+# coding: UTR-8
+
+class Blockchain(object):
+  def __init__(self):
+    self.chain = []
+    self.current_transactions = []
+
+  def new_block(self):
+    # 新しいブロックを作り、チェーンに加える
+    pass
+
+  def new_transaction(self):
+    # 新しいトランザクションをリストに加える
+    pass
+
+  @staticmethod
+  def hash(block):
+    # ブロックをハッシュ化する
+    pass
+
+  @property
+  def last_block(self):
+    # チェーンの最後のブロックをリターンする
+    pass
+
